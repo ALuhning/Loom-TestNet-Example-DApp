cd9b24dd288cdfbff314d6733ce7d986fb993cd3
Aaron Luhning <a.luhning@vitalpoint.ai>
2019-12-24 10:00:42 -0500

    completed example Dapp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Card, Button, Form, Row, Col } from 'react-bootstrap'
+import Contract from './utils/Contract'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.contract = new Contract()
+
+    this.state = {
+      storageValue: 0,
+      error: null,
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    this.contract.loadContract()
+  }  
+
+  async addToSimpleStorage() {
+    if( this.contract.simpleStoreInstance && this.contract.currentUserAddress) {
+      const value = this.storageAmountInput.value
+      this.setState({ loading: true })
+      const result = await this.contract.simpleStoreInstance.methods.set(value).send({ from: this.contract.currentUserAddress })
+      this.setState({
+            storageValue: result.events.NewValueSet.returnValues[0],
+            loading: false
+          })
+    } else {
+      this.setState({
+          error: new Error('simple storage instance not loaded')
+      })
+    }
+  }
+
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+      <Card>
+        <Card.Header as="h3">
+          Simple Storage DApp
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>Storage Value is: {!this.state.loading ? this.state.storageValue : 'Loading...'}</Card.Title>
+              <Form>
+                <Row>
+                  <Col>
+                    <Form.Label>
+                      Storage Amount
+                    </Form.Label>
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      type="number"
+                      placeholder="Enter Number"
+                      ref={c => {this.storageAmountInput = c}}
+                    />
+                  </Col>
+                  <Col>
+                    <Button
+                      variant="primary"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        this.addToSimpleStorage()
+                      }}
+                    >
+                      Set Storage
+                    </Button>
+                  </Col>
+                </Row>
+            </Form>
+          </Card.Body>
+      </Card>
+     </Container>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/contracts/SimpleStore.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract SimpleStore {
+
+    uint value;
+
+    event NewValueSet(uint _value);
+
+    function set(uint _value) public {
+        value = _value;
+        emit NewValueSet(value);
+    }
+
+    function get() public view returns (uint) {
+        return value;
+    }
+}
